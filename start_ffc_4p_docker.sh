05256446c7f33f4a38ed9abdd1ea2197be88f1b6
Kahlil Lau <mortyl0834@gmail.com>
2023-07-14 23:38:37 +0800

    [Feat][script]:add ffc_4p_docker start script

---
+++ start_ffc_4p_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xhost +
+# docker run -it --rm --net=host -v /home/dji/workspace/ffc_4p_ws:/ffc_4p_ws  -v /dev/:/dev/  --privileged -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix  luxonis/depthai-ros:noetic-latest /bin/bash
+docker run -it --rm --net=host -v /home/dji/workspace/ffc_4p_ws:/ffc_4p_ws -v /dev/:/dev/  --privileged -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix  oak_ffc_4p:v1.0 /bin/bash
